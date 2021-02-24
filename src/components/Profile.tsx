d65e3691eb452c36b613f607ec2520c3945e70ef
EstevaoLima <estevao.urbano2@hotmail.com>
2021-02-24 06:31:32 -0300

    initial commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import styles from '../styles/components/Profile.module.css'
+export function Profile(){
+    return(
+
+        <div className={styles.profileContainer}>
+
+            <img src="https://github.com/estevao-lima.png" alt="Estevão Lima"/>
+
+            <div>
+                
+                <strong>Estevão Lima</strong>
+
+                
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level 1
+                </p>
+
+            </div>
+
+        </div>
+
+    );
+}
